6f3683cf9a91be51543e88a28a1b91cd8e7e6837
Cristian-1980 <castro.cris02@gmail.com>
2024-05-14 20:25:50 -0400

    firt

---
+++ assets/js/index.js
@@ -0,0 +1,12 @@
+//Header, botones enviar correo y añadir a favoritos y alerta.
+new bootstrap.Tooltip(myBttns[0]);
+new bootstrap.Tooltip(myBttns[1]);
+
+
+function alerta(evento){
+    event.preventDefault();
+    alert("El correo fue enviado correctamente");
+}
+
+
+
